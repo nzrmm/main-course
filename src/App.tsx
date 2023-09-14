8d0a7a43ac5bc980057572f0f587426a2cda52b2
muhammadgata <iamgata.me@gmail.com>
2023-09-14 09:55:40 +0700

    feat: create header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Button } from "@/components";
+import { cn } from "@/utils/style";
+
 const App = () => {
-  return <div className={"bg-red-500"}>App</div>;
+  return (
+    <div className={"min-h-screen bg-neutral-200 p-8"}>
+      <div className={cn("flex justify-between items-center")}>
+        <p className={cn("text-lg font-semibold")}>Main Course</p>
+        <Button
+          id={`cart-button`}
+          size="sm"
+          variant="outline"
+          className={cn("flex items-center gap-2")}
+        >
+          Keranjang
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
