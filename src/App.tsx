75c2a4f7d73be2a0d78821f3df0183b7bb812b2d
muhammadgata <iamgata.me@gmail.com>
2023-09-14 14:49:21 +0700

    style: set max width 1440px

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Home } from "@/routes";
+import { cn } from "@/utils/style";
 
 const App = () => {
   return (
-    <div className={"antialiased min-h-screen bg-neutral-200 p-8"}>
-      <Home />
+    <div className={cn("antialiased bg-neutral-200")}>
+      <div className={"max-w-[1440px] mx-auto min-h-screen p-8"}>
+        <Home />
+      </div>
     </div>
   );
 };
